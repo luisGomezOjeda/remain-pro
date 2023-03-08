f7ea2ddb56b09505a9a905060ecb0be24e3eb98e
luis gomez <luisgomezojeda110502@gmail.com>
2023-03-08 19:33:03 -0300

    correction nav and add map

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,13 +4,18 @@
 
 
 // nav menu
-const nav_menu = document.querySelector(".nav-menu");
+const $nav_menu = document.querySelector(".nav-menu"),
+$nav_link = document.querySelectorAll(".nav-menu-link");
+
 
 document.addEventListener("click",e=>{
  // button nav menu
  // e.preventDefault();
  if(e.target.matches(".nav-toggle *")){
-  nav_menu.classList.toggle("nav-menu__visible");
+  $nav_menu.classList.toggle("nav-menu__visible");
+ }
+ if(e.target.matches(".nav-menu-link")){
+  $nav_menu.classList.toggle("nav-menu__visible");
  }
 });
 
